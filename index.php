9968ba75affe7790bac8e541af90ca5654105896
NeitherOne1 <100136261+NeitherOne1@users.noreply.github.com>
2022-03-15 00:45:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 !<DOCTYPE html>
 <html>
    <head>
-      <title>My Home Page</title>
-   </head>
-   <body>
-   <h1>Welcome to My Website! I am Ivan James! </h1>
-   <ul>
-        <li><a href="index.php">HOME</a></li>
-        <li><a href="/LT 2/index.html">LEARNING TASK 2</a></li>
-        </ul>
-        </body>
+      <title> My Home Page </title>
+   
+   <styles>
+   body{
+      backgroubd-image:url(images/jpg);
+      background-repeat:no-repeat;
+      background-size:cover;
+      background-position: top center;
+      }
+      </style>
+      </head>
+      <body>
+         <h1 style="font-size:100px; background-color:rgba(255,17,0,73; color:white; text-align:center;>My Top 10 Movies </h1>
+      </body>
+   
  </html
