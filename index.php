517600f2d04e75fd884ef5d2b4a93c2e4b95ff67
NeitherOne1 <100136261+NeitherOne1@users.noreply.github.com>
2022-03-16 01:46:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
       <h1>Welcome to My Website! I am Ivan !</h1>
       <ul>
          <li><a href="index.php">HOME</a></li>
-         <li><a href="/LT2/Learning Task 2/index.html"></a></li>
-         <li><a href="/LT3/Learning Task 3/index.html"></a></li>
+         <li><a href="/LT2/index.html"></a></li>
+         <li><a href="/LT3/index.html"></a></li>
       </ul>
    </body>
    
