3483f8baaef6ea86319054f8e01d5967cfbbda04
NeitherOne1 <100136261+NeitherOne1@users.noreply.github.com>
2022-05-21 03:22:33 +0800

    Add files via upload

---
+++ LT5/js.js
@@ -0,0 +1,12 @@
+ function changeColor(obj) {
+            if (obj.style.color == 'blue') {
+                obj.style.color = 'black';
+            } else {
+                obj.style.color = 'blue';
+            }
+        }
+         
+        function changeBgColor(obj, colorName) {
+            obj.style.backgroundColor = colorName;
+        }
+        
